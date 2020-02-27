328df5606847b93e63b546c060816774a0ec8f4c
Nic Payne <nic.r.payne@gmail.com>
2020-02-27 10:28:38 -0600

    setting up pet form

---
+++ pet-hotel-client/src/components/Form/Form.jsx
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+class AddPetForm extends Component{
+
+    state = {
+        newPet: {
+            name: '',
+            breed: '',
+            color: ''
+        }
+    }
+
+
+
+
+
+    render() {
+
+        return (
+
+
+
+
+
+        )
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+export default connect()(AddPetForm)
